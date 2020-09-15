176304b0006ca1b947dbf3472ba25c1d4ce9abd6
h378915980 <378915980@qq.com>
2020-09-15 05:10:39 -0400

    feat(compress): add compress and extrac files or dir function

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include "widget.h"
 
 #include <QApplication>
-#include "./calibration/position/UPositionMap.h"
 #include <QDebug>
-
+#include "./tool/compress/include/quazip/JlCompress.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Widget w;
-    w.show();
-
+//    Widget w;
+//    w.show();
+   // qDebug()<<JlCompress::compressDir("test.zip","test");
+    JlCompress::extractDir("test.zip","test");
     return a.exec();
 }
